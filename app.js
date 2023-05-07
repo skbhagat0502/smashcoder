231d057717c1f92d423138b3e953591aa751c75c
Sandeep Bhagat <sandeepbh0502@gmail.com>
2023-05-08 03:06:11 +0530

    initial commit

--- app.js
+++ app.js
@@ -6,38 +6,77 @@ const _ = require("lodash");
 const multer = require("multer");
 const app = express();
 const mongoose = require("mongoose");
-app.set('view engine', 'ejs');
+const { marked } = require("marked");
+const slugify = require("slugify");
+const creatDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const dompurify = creatDomPurify(new JSDOM().window);
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb://127.0.0.1:27017/youtubeDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  "mongodb+srv://pandadeveloperofficial:i0UYDy2WqJ0kMLoF@smashcodercluster.mrewaud.mongodb.net/SmashDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const youtubeSchema = new mongoose.Schema({
   thumbnail: { data: Buffer, contentType: String },
   heading: String,
-  description: String
+  description: String,
+  link: String,
 });
 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  createdAt: { type: Date, default: Date.now },
+  description: String,
+  markdown: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  sanitizedHtml: { type: String, required: true },
+});
+
+blogSchema.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
+  }
+  next();
+});
 const youtube = mongoose.model("youtube", youtubeSchema);
+const blog = mongoose.model("blog", blogSchema);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/Images")
+    cb(null, "./public/Images");
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + Date.now() + path.extname(file.originalname));
-  }
+  },
+});
+const storage1 = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/imageblog");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 const upload = multer({ storage: storage });
+const upload1 = multer({ storage: storage1 });
 
 app.get("/", function (request, response) {
   youtube.find({}, (err, posts) => {
     if (err) {
       console.log(err);
-      response.status(500).send('An error occurred', err);
-    }
-    else {
+      response.status(500).send("An error occurred", err);
+    } else {
       response.render("home", { posts: posts });
     }
   });
@@ -46,46 +85,87 @@ app.get("/home", function (request, response) {
   youtube.find({}, (err, posts) => {
     if (err) {
       console.log(err);
-      response.status(500).send('An error occurred', err);
-    }
-    else {
+      response.status(500).send("An error occurred", err);
+    } else {
       response.render("home", { posts: posts });
     }
   });
 });
-app.get("/about", function (request, response) {
-  response.render("about");
-});
+
 app.get("/blogs", function (request, response) {
-  response.render("blogs");
+  blog.find({}, function (err, blogs) {
+    if (err) {
+      console.log(err);
+      response.status(500).send("An error occurred", err);
+    } else {
+      response.render("blogs", { blogs: blogs });
+    }
+  });
+});
+app.get("/blogs/:slug", function (request, response) {
+  blog.findOne({ slug: request.params.slug }, function (err, blogs) {
+    if (blogs) {
+      response.render("posts", { blogs: blogs });
+    } else {
+      response.send("<h1>File does not Exist.</h1>");
+    }
+  });
 });
 app.get("/contact", function (request, response) {
   response.render("contact");
 });
-app.get("/compose", function (request, response) {
-  response.render("compose");
+app.get("/composeyt", function (request, response) {
+  response.render("composeyoutubepost");
+});
+app.get("/composeblogs", function (request, response) {
+  response.render("composeblogs");
 });
 
-app.post("/compose", upload.single("thumbnail"), function (request, response) {
-  var obj = {
-    thumbnail: {
-      data: request.file.filename,
-      contentType: 'image/png'
-    },
-    heading: request.body.heading,
-    description: request.body.description
+app.post(
+  "/composeyt",
+  upload.single("thumbnail"),
+  function (request, response) {
+    var obj = {
+      thumbnail: {
+        data: request.file.filename,
+        contentType: "image/png",
+      },
+      heading: request.body.heading,
+      description: request.body.description,
+      link: request.body.link,
+    };
+    youtube.create(obj, (err, item) => {
+      if (err) {
+        console.log(err);
+      } else {
+        item.save();
+        response.redirect("/");
+      }
+    });
   }
-  youtube.create(obj, (err, item) => {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      item.save();
-      response.redirect('/');
-    }
-  });
-});
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+);
+app.post(
+  "/composeblogs",
+  upload1.array("image", 50),
+  function (request, response) {
+    var obj = {
+      image: {
+        data: request.files.filename,
+        contentType: "image/png",
+      },
+      title: request.body.title,
+      date: request.body.date,
+      description: request.body.description,
+      markdown: request.body.markdown,
+    };
+    blog.create(obj, (err, item) => {
+      if (err) {
+        console.log(err);
+      } else {
+        item.save();
+        response.redirect("blogs");
+      }
+    });
+  }
+);
+app.listen(3000);
--- public/index.js
+++ public/index.js
@@ -1,9 +1,8 @@
-
 const hamburgerMenu = document.querySelector("#hamburger");
 const navigationList = document.querySelector("#main-nav");
 const listItems = document.querySelectorAll(".nav-list li");
 const links = document.querySelectorAll("#mobile-nav-social-icons a");
-const loginButton = document.querySelector("#login-button");
+const contact=document.querySelector("#sendemail");
 
 hamburgerMenu.addEventListener("click", () => {
     navigationList.classList.toggle("hamburgerButtonClicked");
@@ -13,5 +12,14 @@ hamburgerMenu.addEventListener("click", () => {
     links.forEach((link) => {
         link.classList.toggle("fade");
     });
-    loginButton.classList.toggle("fade");
+});
+document.querySelector(".description1").addEventListener("input", (characters) => {
+    const maxlength = characters.target.getAttribute("maxlength");
+    const currentLength = characters.target.value.length;
+    document.querySelector(".counter1").innerHTML = `${currentLength}/${maxlength}`
+});
+document.querySelector(".description2").addEventListener("input", (characters) => {
+    const maxlength = characters.target.getAttribute("maxlength");
+    const currentLength = characters.target.value.length;
+    document.querySelector(".counter2").innerHTML = `${currentLength}/${maxlength}`
 });
